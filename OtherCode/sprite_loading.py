685aedcdfd48d73d0031f55de765f8862dd5bca1
Moritz <96340080+mol42069@users.noreply.github.com>
2022-10-28 21:14:47 +0200

    Create sprite_loading.py

---
+++ OtherCode/sprite_loading.py
@@ -0,0 +1,24 @@
+import pygame as py
+from enum import Enum
+
+# 1920 x 1080  = 64 x 36
+
+
+# ------------------------------------------------- load the files --------------------------------------------------- #
+
+
+def load_all():
+    sprites = []
+    play_img = py.image.load("./SpriteSheets_etc/temp_Play.png")
+
+    sprites.insert(0, play_img)
+
+    return sprites
+
+
+# ----------------------------------------- enums to access sprites easy --------------------------------------------- #
+
+
+class Sprite(Enum):
+    PLAY = 0
+    P_Head_R = 1
