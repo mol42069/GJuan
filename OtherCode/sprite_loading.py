bd20132a52d0ba98f399296d9f22bf6f7e178362
Moritz <96340080+mol42069@users.noreply.github.com>
2022-10-29 11:18:24 +0200

    Update sprite_loading.py

--- OtherCode/sprite_loading.py
+++ OtherCode/sprite_loading.py
@@ -9,16 +9,30 @@ from enum import Enum
 
 def load_all():
     sprites = []
-    play_img = py.image.load("../SpriteSheets_etc/temp_Play.png")
+    play_img = py.image.load("D:/FH/3.Semester/Python/Code/GJuan/SpriteSheets_etc/temp_Play.png")
+    player_sheet = py.image.load('D:/FH/3.Semester/Python/Code/GJuan/SpriteSheets_etc/PlayerModels.png')
 
     sprites.insert(0, play_img)
+    sprites.insert(1, extract_ss(player_sheet))
 
     return sprites
 
+
+# ------------------------------------------ extract from sprite-sheet ----------------------------------------------- #
+
+
+def extract_ss(sprite_sheet):
+    back = []
+    for i in range(1, 100):
+        for o in range(1, 100):
+            sprite_sheet.set_clip(py.Rect((o - 1) * 30 + o, (i - 1) * 30 + i, 30, 30))
+            back.append(sprite_sheet.subsurface(sprite_sheet.get_clip()))
+
+    return back
 
 # ----------------------------------------- enums to access sprites easy --------------------------------------------- #
 
 
 class Sprite(Enum):
     PLAY = 0
-    P_Head_R = 1
+    PLAYER = 1
